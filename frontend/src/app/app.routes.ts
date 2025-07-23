4dbf262bde31e3ba5a58179dfe837555066c98df
mariokreitz <mario.kreitz@web.de>
2025-07-24 01:23:54 +0200

    feat: Adds header and footer components
    Adds header and footer components to the main application layout.
    Also, it introduces authentication guards to protect routes.
    'noAuthGuard' redirects logged-in users from public pages like home and login.
    'authGuard' ensures only authenticated users can access the dashboard.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,17 +1,22 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
+import { noAuthGuard } from './core/guards/no-auth-guard';
 
 export const routes: Routes = [
     {
         path: '',
         loadComponent: () => import('./pages/home/home').then(m => m.Home),
+        canActivate: [ noAuthGuard ],
     },
     {
         path: 'login',
         loadComponent: () => import('./pages/login/login').then(m => m.Login),
+        canActivate: [ noAuthGuard ],
     },
     {
         path: 'dashboard',
         loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard),
+        canActivate: [ authGuard ],
     },
     {
         path: '**',
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './core/components/footer/footer';
+import { Header } from './core/components/header/header';
 
 @Component({
     selector: 'app-root',
     imports: [
         RouterOutlet,
+        Header,
+        Footer,
     ],
     templateUrl: './app.html',
     styleUrl: './app.css',
