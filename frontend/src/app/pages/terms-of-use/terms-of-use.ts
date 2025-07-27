c83fd35ba6e197aaabec1f5c4b0c296624c1828b
mariokreitz <mario.kreitz@web.de>
2025-07-27 02:42:06 +0200

    feat(terms-of-use): implement terms of use page layout

--- frontend/src/app/pages/terms-of-use/terms-of-use.ts
+++ frontend/src/app/pages/terms-of-use/terms-of-use.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-terms-of-use',
-  imports: [],
-  templateUrl: './terms-of-use.html',
-  styleUrl: './terms-of-use.css'
+    selector: 'app-terms-of-use',
+    imports: [
+        RouterLink,
+    ],
+    templateUrl: './terms-of-use.html',
+    styleUrl: './terms-of-use.css',
 })
 export class TermsOfUse {
 
